f7737d6533818defc81d276bec2fed8d0e57db7d
Michael Lund <michaelwalund@gmail.com>
2021-10-13 09:41:26 +0200

    Started with Python

---
+++ test/test.py
@@ -0,0 +1,12 @@
+print("Hello Machine Learning World!")
+
+test = "Hello World"
+
+if (test == "Hello World"):
+    print(test)
+
+
+myFloat = float(3)
+
+print(myFloat)
+print(str(myFloat) + " " + test)
